7a0194d1ca57528c484f43f052da40a7539e9a1c
Veronica Minciuna <veronicaminci@gmail.com>
2022-02-08 00:06:15 -0500

    fixed error in fetch request

--- script.js
+++ script.js
@@ -8,19 +8,29 @@ const count = 10;
 const apiKey = 'wSjR-G0YCs0FtAVt--AIw389L0-Que9X6Xs1ks8UdkE';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+function setAttributes(element, attributes) {
+    for(const key in attributes) {
+        element.setAttribute(key, attributes[key])
+    }
+}
+
 //Create elements for links & photos, add to DOM
 function displayPhotos() {
     //run function for each obj in photoArr
     photosArr.forEach((photo)=> {
         //create <a> to ink to unsplash
         const item = document.createElement('a');
-        item.setAttribute('href',photo.links.html);
-        item.setAttribute('target','_blank');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        })
         //create, <img> for photo
         const img = document.createElement('img');
-        img.setAttribute('src',photo.urls.regular);
-        img.setAttribute('alt', photo.alt_decription);
-        img.setAttribute('title', photo.alt_decription);
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        })
         //put <img> inside <a>, then put both inside imageContainer Elem
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -32,10 +42,9 @@ async function getPhotos() {
     try {
        const response = await fetch(apiUrl);
        photosArr = await response.json();
-       console.log(photosArr)
        displayPhotos();
     } catch(error) {
     }
 }
 
-getPhotos()
+getPhotos();
